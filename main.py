b46cfd8e4dc96bde28c274a09c2450bac5576608
Ibrohimov95 <yusufibrohimov58@gmail.com>
2025-11-01 18:58:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Sun'iy intellekt asoslari - 4-laboratoriya
+# Muallif: Yusuf Ibrohimov
+
+def salom_ber(ism):
+    print(f"Salom, {ism}! GitHub orqali Python loyihasi yaratildi.")
+
+salom_ber("Yusuf")
